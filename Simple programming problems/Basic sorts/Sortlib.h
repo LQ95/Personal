e3bd0f42d61e3a77ea4abdd277f39a39b9699ad7
LQ95 <38260780+LQ95@users.noreply.github.com>
2018-11-20 02:26:58 +0100

    Basic sorts folder created

---
+++ Simple programming problems/Basic sorts/Sortlib.h
@@ -0,0 +1,8 @@
+#ifndef SortsH
+#define SortsH
+void SelectionSort();
+void InsertionSort();
+void MergeSort();
+void QuickSort();
+void StoogeSort();
+#endif
---
+++ Simple programming problems/Basic sorts/Sorts.c
@@ -0,0 +1,21 @@
+#include "Sortlib.h"
+void SelectionSort()
+{
+	
+}
+void InsertionSort()
+{
+	
+}
+void MergeSort()
+{
+	
+}
+void QuickSort()
+{
+	
+}
+void StoogeSort()
+{
+	
+}
