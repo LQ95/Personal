f3dee2081ca5e8c68769a72900eb65f55f9c6d62
LQ95 <38260780+LQ95@users.noreply.github.com>
2019-04-08 21:44:27 +0200

    Partial refacorting,adding a Java exercise on generics I did for my uni

---
+++ Java/Uni stuff/MxBox/MxBox.java
@@ -0,0 +1,72 @@
+
+import java.util.*;
+import javax.activation.*;
+public class MxBox{
+		public static void main(String[] args) throws UnsupportedDataTypeException
+		{
+			MaxBox<Integer> bb3 = new MaxBox<Integer>();
+			MaxBox<String> bb1 = new MaxBox<String>();
+			MaxBox<String> bb2 = new MaxBox<String>(new Comparator<String>() {
+				public int compare(String a, String b)
+				{
+						return a.length() - b.length();
+				}
+			});
+			bb1.insert ("dodici");
+			bb1.insert ("sette");
+			bb2.insert ("dodici");
+			bb2.insert ("sette");
+			System.out.println(bb1.getMax());
+			System.out.println(bb2.getMax());
+		}
+	
+}
+class MaxBox <T> {
+
+	private T Content;
+	private Comparator<T> TypeComparator;
+	public T getMax()
+		{
+			return Content;
+		}
+	public void insert(T element) throws UnsupportedDataTypeException //sostituisce se ciò che al momento è contenuto risulta minore dell'elemento inserito
+		{
+			if (this.Content == null)
+				this.Content=element;
+			else if(this.TypeComparator!=null)
+				{
+					if (this.TypeComparator.compare(element,this.Content)>0)
+					this.Content=element;
+				}
+			else if (element instanceof Comparable)
+				{ 
+					Comparable<T> TComp=(Comparable<T>) element ;
+					if (TComp.compareTo(this.Content) >0)
+					this.Content=element;
+					return;
+				}
+			else throw new UnsupportedDataTypeException("inserito con un tipo di dato non comparabile ");
+		}
+
+	public MaxBox()
+		{
+			Content=null;
+		}
+		
+	public MaxBox(T content)throws UnsupportedDataTypeException
+		{
+			if (content instanceof Comparable)	
+				this.Content=content;
+			else throw new UnsupportedDataTypeException("inserito con un tipo di dato non comparabile nel costruttore");
+		}
+			
+	public MaxBox(Comparator<T> Comp,T content)
+		{
+			Content=content;
+			this.TypeComparator=Comp;
+		}	
+	public MaxBox(Comparator<T> Comp)
+		{
+			this.TypeComparator=Comp;
+		}
+}
