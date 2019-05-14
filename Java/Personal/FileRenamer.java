51397a384be36409e0ef3282b2254c8b3f4eabaf
LQ95 <38260780+LQ95@users.noreply.github.com>
2019-05-14 20:40:30 +0200

    adding asmall window and an empty ActionListener

--- Java/Personal/FileRenamer.java
+++ Java/Personal/FileRenamer.java
@@ -2,10 +2,22 @@
 //has a basic UI too
 import java.awt.*;
 import javax.swing.*;
+import java.io.File;
 public class FileRenamer{
 	public static void main(String[] args)
 	{
-
+		JFrame Window1=new JFrame();
+		Window1.setSize(640,480);
+		Window1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Window1.setVisible(true);
+	}
+	
+	class LocalActionListener implements ActionListener
+	{
+		void actionPerformed(ActionEvent e)
+		{
+			
+		}
 	}
 }
 
