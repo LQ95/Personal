cf255a0951719cbe894b16525f540d7495d59b75
LQ95 <38260780+LQ95@users.noreply.github.com>
2019-05-26 14:02:32 +0200

    Finished and working correctly after every test

--- Java/Uni stuff/reverseList/Inversion.java
+++ Java/Uni stuff/reverseList/Inversion.java
@@ -4,17 +4,41 @@ public class Inversion
 	public static void main(String[] args)
 	{
 		Inverter Inv=new Inverter();
+		//testing
 		List<Integer> List1= new LinkedList<Integer>();
+		List1.add(new Integer(50));
+		List1.add(new Integer(5000));
+		PrintList(List1);
 		List<Object> List2= new ArrayList<Object>();
+		List2.add(new Boolean(true));
+		List2.add(new Thread());
+		PrintList(List2);
 		List<Exception> List3= new LinkedList<Exception>();
+		List3.add(new IllegalArgumentException());
+		List3.add(new ClassCastException());
+		PrintList(List3);
 		List<String> List4= new ArrayList<String>();
+		List4.add("Sofferenza");
+		List4.add("Java");
+		PrintList(List4);
 		List1=Inv.<Integer>reverseList(List1);
+		PrintList(List1);
 		List2=Inv.<Object>reverseList(List2);
+		PrintList(List2);
 		List3=Inv.<Exception>reverseList(List3);
+		PrintList(List3);
 		List4=Inv.<String>reverseList(List4);
+		PrintList(List4);
 	}
 	
-
+	static <T> void  PrintList(List<T> L)
+	{
+		Iterator<T> i=L.iterator();
+		for(T element:L)
+			{
+			System.out.println(element);
+			}
+	}
 }
 
 class Inverter
@@ -23,12 +47,10 @@ class Inverter
 		{
 		T temp;
 		int size=L.size();
-		Iterator<T> i=L.iterator();
 		for(int j=0;j<size;j++)
 			{
-				temp=i.next();
-				i.remove();
-				L.add(temp);
+				temp=L.remove(j); //remove from current position and add to head
+				L.add(0,temp);
 			}
 		return L;
 		}
