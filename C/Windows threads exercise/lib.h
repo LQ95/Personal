31c0d2c8e5279db3c24ae19b25764f46baae5e84
LQ95 <38260780+LQ95@users.noreply.github.com>
2019-07-01 21:58:34 +0200

    adding more stuff to the windows threads test

---
+++ C/Windows threads exercise/lib.h
@@ -0,0 +1,8 @@
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <wingdi.h>
+
+typedef struct TD{
+HWND Handle;
+}ThreadData;
--- C/Windows threads exercise/main.c
+++ C/Windows threads exercise/main.c
@@ -1,12 +1,18 @@
-#include <windows.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include "lib.h"
+//Testing windows GDI,can I make a game out of it?
 HANDLE T1,T2;
 DWORD ThreadID;
-
+RECT Gamefield;
 DWORD WINAPI TestFunction(LPVOID arg)
 {
-	printf("Thread 1\n");
+	PAINTSTRUCT ps;
+	ThreadData *TD1=arg;
+	HDC context=GetDC(TD1->Handle);
+	context=BeginPaint(TD1->Handle,&ps);
+	GetUpdateRect(TD1->Handle,&Gamefield,FALSE);
+	//I'll add something here later on
+	EndPaint(TD1->Handle,&ps);
+	printf("Thread 1 disegna\n");
 }
 
 DWORD WINAPI TestFunction2(LPVOID arg)
@@ -16,7 +22,10 @@ DWORD WINAPI TestFunction2(LPVOID arg)
 
 int main()
 {
-	T1=CreateThread(NULL,0,(LPTHREAD_START_ROUTINE)TestFunction,0,0,&ThreadID);
+	HWND MainWindow=GetActiveWindow();
+	ThreadData *TD1=malloc(sizeof(ThreadData));
+	TD1->Handle=MainWindow;
+	T1=CreateThread(NULL,0,(LPTHREAD_START_ROUTINE)TestFunction,TD1,0,&ThreadID);
 	T2=CreateThread(NULL,0,(LPTHREAD_START_ROUTINE)TestFunction2,0,0,&ThreadID);
 	system("pause");
 	return 0;
