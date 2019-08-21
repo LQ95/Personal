0ff7f6523657a7938b50935d105daab348511d36
LQ95 <38260780+LQ95@users.noreply.github.com>
2019-08-21 13:11:42 +0200

    File renamer,further implementation

---
+++ Java/Personal/file renamer/FileRenamer.java
@@ -0,0 +1,67 @@
+//takes eevery file in the directory and renames it in order,with added filenames
+//has a basic UI too
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.io.File;
+public class FileRenamer{
+	private static Implementation Ren;
+	private static JFrame Window1=new JFrame("A little silly Mass File Renamer");
+	private	static JTextField TField=new JTextField("Enter directory here...",20);
+	private	static JButton Confirm=new JButton("Confirm");
+	private	static JButton Browse=new JButton("Browse");
+	private	static JFileChooser ChooseDialog= new JFileChooser();
+	private	File SelectedDirectory;
+	ActionListener BrowseListener= new ActionListener(){
+		public void actionPerformed(ActionEvent e)
+				{
+					ChooseDialog.showOpenDialog(Window1);
+					SelectedDirectory=ChooseDialog.getSelectedFile();
+					TField.setText(SelectedDirectory.getPath());
+				}
+			};
+	public static void main(String[] args)
+	{
+		//declarations
+		Ren= new Implementation();
+		//Settings
+		ChooseDialog.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+		Confirm.setBounds(20,30,50,30);
+		Window1.setSize(640,480);
+		Window1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Window1.setVisible(true);
+		//Action Listeners
+		Confirm.addActionListener(new ActionListener(){
+		public void actionPerformed(ActionEvent e)
+				{
+					Ren.rename(TField.getText());
+				}
+			}
+		);
+		Browse.addActionListener(BrowseListener);
+		//everything is put inside the window
+		Container windowContainer=Window1.getContentPane();
+		windowContainer.setLayout(new GridLayout(5,2,23,40));
+		windowContainer.add(TField);
+		windowContainer.add(Browse);
+		windowContainer.add(Confirm);
+	}
+	
+
+}
+
+class Implementation {
+	public void rename(String BaseForRenaming){
+	 //retrieve current working directory
+		File WorkingDir= new File(System.getProperty("user.dir"));
+		File ToBeOrdered[] =WorkingDir.listFiles();
+		int size=ToBeOrdered.length;
+		int strLen=BaseForRenaming.length();
+		StringBuilder NewFilename=new StringBuilder(BaseForRenaming);
+		for(int i=0;i<size;i++)
+		{
+			NewFilename.insert(strLen,i);
+			ToBeOrdered[i].renameTo(new File(NewFilename.toString()));
+		}
+	}
+}
