d131da98e7460e6331ffa881064af2637595b7bc
LQ95 <38260780+LQ95@users.noreply.github.com>
2019-10-02 16:07:44 +0200

    Adding some notes,trying to find a more efficient implementation

--- C/Simple programming problems/resolve to 100/main.c
+++ C/Simple programming problems/resolve to 100/main.c
@@ -2,10 +2,14 @@
 Write a program that outputs all possibilities to put + or - or nothing between the numbers 1,2,…,9
  (in this order) such that the result is 100. For example 1 + 2 + 3 - 4 + 5 + 6 + 78 + 9 = 100.
 */
+/*notes: checking for every possible operation with three operands on every number from 1 to 9 would need 3^8 cycles. 
+however we can cut that down,excluding every cicle without an AddDigit operation in it witch subracts 2^8 from the previously mentioned 3^8
+
+*/
 #include "lib.h"
 int main(){
 	Operator OpArray[3]={sum,subtract,addDigit};
-	Operator OpList[9];
+	Operator OpList[8];
 	Operator CurrentFunc;
 	srand(time(NULL));
 	int i;
