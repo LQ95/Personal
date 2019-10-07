94c941243dd3bd8042a9d3ea0181f3a2f3b51bdb
LQ95 <38260780+LQ95@users.noreply.github.com>
2019-10-07 21:24:14 +0200

    small fix

--- C/Simple programming problems/resolve to 100/main.c
+++ C/Simple programming problems/resolve to 100/main.c
@@ -3,7 +3,7 @@ Write a program that outputs all possibilities to put + or - or nothing between 
  (in this order) such that the result is 100. For example 1 + 2 + 3 - 4 + 5 + 6 + 78 + 9 = 100.
 */
 /*notes: checking for every possible operation with three operands on every number from 1 to 9 would need 3^9-1 cycles. 
-however we can cut that down,excluding every cicle without an AddDigit operation in it witch subracts 2^9-1 from the previously mentioned 3^9-1
+however we can cut that down,excluding every cicle without a Fuse operation in it witch subracts 2^9-1 from the previously mentioned 3^9-1
 
 */
 #include "lib.h"
@@ -13,30 +13,33 @@ int main(){
 	int last=pow(3,9)-1;
 	int iteration=0;
 	srand(time(NULL));
-	int i,sum,size,OpIndex;
+	int i,Operator,sum,size,OpIndex;
 	while(iteration<last){
 	size=1;
 	sum=0;
+	OpIndex=0;
 	MakeOpList(OpList,iteration);
-	for (i=1;i<8;i++)  //The array of numbers is first properly transformed,and then the calculation is made
+	for (i=1;i<9;i++)  //The array of numbers is first properly transformed,and then the calculation is made
 		{
-			OpIndex=OpList[size-1];
+			Operator=OpList[OpIndex];
+			OpIndex++;
 			//different operations modify the array of operands differently
-			if(OpIndex==2)//if Fuse is selected,the result is added to the number array directly
+			if(Operator==2)//if Fuse is selected,the result is added to the number array directly
 				{
-					if(size>1) 
+					if(i>1) 
 						numbers[size-1]=Fuse(numbers[size-1],i+1);
 					else 
 						numbers[size-1]=Fuse(i,i+1);
+					size--;
 					i++;
 				}
-			else if	(OpIndex==1) 
+			else if	(Operator==1) 
 				numbers[size]=-(i+1);
-			else if	(OpIndex==0) 
+			else if	(Operator==0) 
 				numbers[size]=i+1;
 			size++;
 		}
-		//operations are exectued after the number array is made
+		//the calculation is made after the array is transformed
 	for (i=0;i<size;i++)
 		{
 			sum+=numbers[i];
