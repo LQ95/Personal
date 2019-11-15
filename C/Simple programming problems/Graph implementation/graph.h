ffd0eebf657db394b3a6c49a425199931ad13ccd
LQ95 <38260780+LQ95@users.noreply.github.com>
2019-11-15 20:39:04 +0100

    progress on graphs

--- C/Simple programming problems/Graph implementation/graph.h
+++ C/Simple programming problems/Graph implementation/graph.h
@@ -1,2 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+typedef struct e{
+	struct e *next;
+	int node;
+	int info;
+}edge;
+//Edge list operations here
+
+typedef struct g{
+	edge **edges;
+	int nodes;
+}graph;
+
+//Graph operations here
