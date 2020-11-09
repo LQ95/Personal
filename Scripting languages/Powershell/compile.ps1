5924dd1944bbf8ef29d6a9b0a0a4ce9888f495b1
LQ95 <38260780+LQ95@users.noreply.github.com>
2020-11-09 16:28:21 +0100

    uploading almost functional Powershell GUI script

---
+++ Scripting languages/Powershell/compile.ps1
@@ -0,0 +1,88 @@
+#Compile function
+function Compile($Text,$DropDown){
+
+if(Test-Path -Path $Text -PathType leaf -eq $False)
+	{
+		[System.Windows.MessageBox]::Show('Invalid Path ')
+	}
+else {
+    if($DropDown -eq 0)
+           {
+	           gcc -o test.exe $Text
+	       }
+    else {
+            g++ -o test.exe $Text
+        }
+    }
+}
+
+#Add windows Forms
+Add-Type -AssemblyName System.Windows.Forms
+
+#Create a new form
+$CompileForm = New-Object system.Windows.forms.Form
+
+#Define size.title and background color
+$CompileForm.ClientSize = "600,400"
+$CompileForm.text ="Compilation"
+$CompileForm.BackColor = "#0077cc"
+
+#adding label
+$TitleLabel = New-Object system.Windows.forms.Label
+$TitleLabel.text = "Please select a file path and a compiler."
+$TitleLabel.AutoSize = $true
+
+
+#Positioning label
+$TitleLabel.Font = "Microsoft Sans Serif,24"
+
+#Other smaller labels
+$PathLabel = New-Object System.Windows.forms.Label
+$Compiler = New-Object System.Windows.Forms.ComboBox
+$CompilerLabel = New-Object System.Windows.Forms.Label
+$CompPath = New-Object System.Windows.Forms.TextBox
+
+$PathLabel.text = "Path:"
+$PathLabel.AutoSize = $true
+$PathLabel.width =100
+$PathLabel.height =100
+$PathLabel.Font = "Microsoft Sans Serif,12"
+
+$Compiler.AutoSize = $true
+$Compiler.Font = "Microsoft Sans Serif,12"
+
+$CompilerLabel.text = "Compiler:"
+$CompilerLabel.AutoSize = $true
+$CompilerLabel.width = 100
+$CompilerLabel.height = 100
+$CompilerLabel.Font = "Microsoft Sans Serif,12"
+
+$CompPath.AutoSize = $false
+$CompPath.width = 600
+$CompPath.height = 24
+$CompPath.Font = "Microsoft Sans Serif,12"
+
+#Add compilers and select the default one
+@("C (gcc)","C++ (g++)")|ForEach-Object{[void]$Compiler.Items.Add($_)}
+$Compiler.SelectedIndex = 0
+
+#button handling
+$CompileButton = New-Object System.Windows.Forms.Button
+$CompileButton.BackColor = "#a4ba67"
+$CompileButton.text = "Compile"
+$CompileButton.width = 90 
+$CompileButton.height= 30
+$CompileButton.Font= "Microsoft Sans Serif,10"
+$CompileButton.Add_Click({ Compile($CompileForm.controls[$CompPath].Text,$CompileForm.controls[$Compiler].SelectedItem) })
+#Positioning 
+$TitleLabel.location=New-Object System.Drawing.Point(0,0);
+$PathLabel.location=New-Object System.Drawing.Point(0,75);
+$CompPath.location=New-Object System.Drawing.Point(0,100);
+$CompilerLabel.location=New-Object System.Drawing.Point(0,200);
+$Compiler.location=New-Object System.Drawing.Point(75,200);
+$CompileButton.location=New-Object System.Drawing.Point(250,300);
+
+#Add elements to form
+$CompileForm.controls.AddRange(@($TitleLabel,$PathLabel,$CompPath,$CompilerLabel,$Compiler,$CompileButton)) 
+#Display the form
+[void]$CompileForm.ShowDialog()
